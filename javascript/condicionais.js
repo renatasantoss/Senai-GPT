d3cd3c10989dabad7cd6f8b03128643b8b0a3444
Renata Santos <renatarhc@outlook.com>
2025-08-22 21:50:04 -0300

    JavaScript - Inicio

---
+++ javascript/condicionais.js
@@ -0,0 +1,17 @@
+/// Exercício 1
+
+let idade = prompt("Qual é a idade?");
+
+if (idade >= 18) {
+
+    alert("Parabéns! Você é maior de idade.");
+
+}   else {
+
+    alert("Erro")
+
+}
+
+/// Exercício 2
+
+let número = prompt("")
---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+// console.log("Hello World!");
+
+// const valorfixo = "Renata" // Valor fixo // Hardcore
+
+
+
+// let nome = prompt ("Qual seu nome?");
+// let idade = prompt ("Qual sua idade?");
+
+// alert(" Meu nome é " + nome + " , tenho " + idade + " anos " );
+
+
+let idade = prompt("Qual é a idade?");
+
+if (idade >= 18) {
+
+    alert("Parabéns! Você é maior de idade.");
+
+}   else {
+
+    alert("Erro")
+
+}
