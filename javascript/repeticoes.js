aa78d341aa830b226fc557c368f271e22d9b5150
Renata Santos <renatarhc@outlook.com>
2025-08-27 21:58:51 -0300

    Exercicios repeticoes JS

---
+++ javascript/repeticoes.js
@@ -0,0 +1,52 @@
+// console.log("FOR")
+// for (let contadora = 1; contadora <= 5; contadora++) {
+
+//     console.log(contadora);
+
+// }
+
+// console.log("WHILE")
+// let contadoraWhile = 1;
+// while (contadoraWhile <= 5) {
+
+//     console.log(contadoraWhile);
+
+//     contadoraWhile++;
+
+// }
+
+// Exercicio 1
+// for (let contadora = 1; contadora <=10; contadora++) {
+
+//     console.log(contadora);
+
+// }
+
+// Exercicio 2
+
+// let número = prompt("Digite um número.");
+
+// for (let contadora = 1; contadora <=10; contadora++) {
+
+//     let resultado = contadora * número
+
+//     console.log(resultado);
+
+// }
+
+// Exercicio 3
+
+let N = prompt("Digite um número.");
+N = Number(N);
+
+    let soma = 0;
+
+for (let contadora = 1; contadora <= N; contadora++) {
+
+    soma = soma + contadora;
+
+    // console.log("A soma dos primeiros", N, "Números naturais é", soma);
+
+}
+
+console.log(soma)
