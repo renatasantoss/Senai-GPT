dde9f95db0d7d43f76e2c89055328fc8f52b3ba6
Renata Santos <renatarhc@outlook.com>
2025-08-29 21:54:41 -0300

    Exercicios Html+CSS+JS

--- javascript/condicionais.js
+++ javascript/condicionais.js
@@ -1,157 +1,201 @@
 // /// Exercício 1
+function exercicio01() {
 
-// let idade = prompt("Qual é a idade?");
+    let idade = prompt("Qual é a idade?");
 
-// if (idade >= 18) {
+    if (idade >= 18) {
 
-//     alert("Parabéns! Você é maior de idade.");
+        alert("Parabéns! Você é maior de idade.");
 
-// }   else {
+    } else {
 
-//     alert("Erro")
+        alert("Erro! Você é menor de idade")
 
-// }
+    }
+
+    adicionarlog();
+
+}
 
 /// Exercício 2
+function exercicio02() {
 
-// let número = prompt("Digite um número.")
+    let número = prompt("Digite um número.")
 
-// if (número > 0) {
+    if (número > 0) {
 
-//     alert ("O número é positivo.")
+        alert("O número é positivo.")
 
-// }
-//     else { 
+    }
+    else {
 
-//     alert("O número é negativo")
+        alert("O número é negativo")
 
-//     }
+    }
+
+    adicionarlog();
+
+}
 
 /// Exercício 3
+function exercicio03() {
 
-//  let nota = prompt("Digite a nota do aluno");
+    let nota = prompt("Digite a nota do aluno");
 
-//  if (nota >= 60) {
+    if (nota >= 60) {
 
-//     alert("Parabéns! Você está aprovado.");
+        alert("Parabéns! Você está aprovado.");
 
-//  }
-//     else {
+    }
+    else {
 
-//     alert("Você está reprovado.");
+        alert("Você está reprovado.");
 
-//     }
+    }
+
+    adicionarlog();
+
+}
 
 /// Exercício 4
+function exercicio04() {
 
-//  let número = prompt("Digite um número.");
+    let número = prompt("Digite um número.");
 
-//  if (número > 0) {
+    if (número > 0) {
 
-//     alert ("O número é positivo.");
+        alert("O número é positivo.");
 
-//  } else {
+    } else {
 
-// if (número == 0) {
+        if (número == 0) {
 
-//     alert ("O número é zero");
+            alert("O número é zero");
 
-// } else {
+        } else {
 
-//     alert("O número é negativo");
+            alert("O número é negativo");
 
-// } 
-// }
+        }
+    }
+
+    adicionarlog();
+
+}
 
 /// Exercício 5
+function exercicio05() {
 
-// let idade = prompt("Digite sua idade");
+    let idade = prompt("Digite sua idade");
 
-//     if (idade > 0 && idade <= 12) {
+    if (idade > 0 && idade <= 12) {
 
-// alert("Você é uma criança.");
+        alert("Você é uma criança.");
 
-// } else {
+    } else {
 
-//     if (idade > 12 && idade <= 17) {
+        if (idade > 12 && idade <= 17) {
 
-// alert("Você é um adolecente");
+            alert("Você é um adolecente");
 
 
-// } else {
+        } else {
 
-//     if (idade >= 18) {
+            if (idade >= 18) {
 
-// alert("Você é um adulto"); 
+                alert("Você é um adulto");
 
-//     }
+            }
 
-// }
-// }
+        }
+    }
+
+    adicionarlog();
+
+}
 
 /// Exercício 6
+function exercicio06() {
 
-// let número = prompt("Digite um número.");
+    let número = prompt("Digite um número.");
 
-//     if (número % 2 == 0) {
+    if (número % 2 == 0) {
 
-// alert("O número é par")
+        alert("O número é par")
 
-// } else {
+    } else {
 
-// alert("O número é ímpar.")
+        alert("O número é ímpar.")
 
-// }
+    }
+
+    adicionarlog();
+
+}
 
 /// Exercício 1 - Intermediário
+function exercicioint01() {
 
-//  let número1 = parseFloat(prompt("Digite um número."));
-//  let número2 = parseFloat(prompt("Digite outro número."));
-//  let operacao = prompt("Digite a operação (+, -, *, /):");
+    let número1 = parseFloat(prompt("Digite um número."));
+    let número2 = parseFloat(prompt("Digite outro número."));
+    let operacao = prompt("Digite a operação (+, -, *, /):");
 
-//  let resultado
+    let resultado
 
-//     if (operacao === "+") {
-//         resultado = número1 + número2
-//     } else if  (operacao === "-") {
-//         resultado = número1 - número2
-//     } else if  (operacao === "*") {
-//         resultado = número1 * número2
-//     } else if (operacao === "/") {
-//         resultado = número1 / número2
+    if (operacao === "+") {
+        resultado = número1 + número2
+    } else if (operacao === "-") {
+        resultado = número1 - número2
+    } else if (operacao === "*") {
+        resultado = número1 * número2
+    } else if (operacao === "/") {
+        resultado = número1 / número2
 
-//     }
+    }
 
-//     alert("Resultado: " + resultado);
+    alert("Resultado: " + resultado);
+
+    adicionarlog();
+
+}
 
 /// Exercício 2 - Intermediário
+function exercicioint02() {
 
-//  let valor = parseFloat(prompt("Valor da compra."));
+    let valor = parseFloat(prompt("Valor da compra."));
 
-//     if (valor > 100) {
+    if (valor > 100) {
 
-//  let desconto = valor * 0.10;
-//  let valorComDesconto = valor - desconto;
+        let desconto = valor * 0.10;
+        let valorComDesconto = valor - desconto;
 
-//     alert("Valor original: R$ " + valor.toFixed(2) + "\nDesconto de 10%: R$ " + desconto.toFixed(2) + "\nValor com desconto: R$ " + valorComDesconto.toFixed(2));
+        alert("Valor original: R$ " + valor.toFixed(2) + "\nDesconto de 10%: R$ " + desconto.toFixed(2) + "\nValor com desconto: R$ " + valorComDesconto.toFixed(2));
 
-//     } else {
-//     alert("Valor da compra: R$ " + valor.toFixed(2) + "\nSem desconto, valor normal.");
-// }
+    } else {
+        alert("Valor da compra: R$ " + valor.toFixed(2) + "\nSem desconto, valor normal.");
+    }
+
+    adicionarlog();
+
+}
 
 /// Exercício 3 - Intermediário
+function exercicioint03() {
 
-// let usuario = prompt("Insira seu usuário.");
-// let senha = prompt("Insira sua senha.");
+let usuario = prompt("Insira seu usuário.");
+let senha = prompt("Insira sua senha.");
 
-// if (usuario == "admin" && senha == "1234") {
+if (usuario == "admin" && senha == "1234") {
 
-//     alert("Login bem-sucedido");
+    alert("Login bem-sucedido");
 
-// } else {
+} else {
 
-//     alert("Acesso negado");
+    alert("Acesso negado");
 
-// }
+}
 
- 
+    adicionarlog();
+
+}
+
--- javascript/repeticoes.js
+++ javascript/repeticoes.js
@@ -1,21 +1,51 @@
+function adicionarlog() {
+
+    let divLogs = document.querySelector(".logs"); // Seleciona a div
+
+    let tagP = document.createElement("p"); //Cria a tag p
+
+    tagP.innerHTML = "Exercício executado em " + new Date().toLocaleDateString() + " as " + new Date().toLocaleTimeString();    
+
+    divLogs.appendChild(tagP); // Acrescenta a tagP dentro da divLogs.
+
+}
+
 // console.log("FOR")
-// for (let contadora = 1; contadora <= 5; contadora++) {
+function exerciciorep01for() {
 
-//     console.log(contadora);
+    for (let contadora = 1; contadora <= 10; contadora++) {
 
-// }
+        console.log(contadora);
+
+    }
+
+    alert("Verificar exercício no console.")
+
+    adicionarlog();
+
+}
+
 
 // console.log("WHILE")
-// let contadoraWhile = 1;
-// while (contadoraWhile <= 5) {
+function exerciciorep01while() {
 
-//     console.log(contadoraWhile);
+    let contadoraWhile = 1;
+    while (contadoraWhile <= 10) {
 
-//     contadoraWhile++;
+        console.log(contadoraWhile);
 
-// }
+        contadoraWhile++;
+
+    }
+
+    alert("Verificar exercício no console.")
+
+    adicionarlog();
+
+}
 
 // Exercicio 1 - Básico
+
 // for (let contadora = 1; contadora <=10; contadora++) {
 
 //     console.log(contadora);
@@ -23,49 +53,75 @@
 // }
 
 // Exercicio 2 - Básico
+function exercicio02rep() {
 
-// let número = prompt("Digite um número.");
+    let número = prompt("Digite um número.");
 
-// for (let contadora = 1; contadora <=10; contadora++) {
+    for (let contadora = 1; contadora <= 10; contadora++) {
 
-//     let resultado = contadora * número
+        let resultado = contadora * número
 
-//     console.log(resultado);
+        console.log(resultado);
 
-// }
+    }
+
+    alert("Verificar exercício no console.")
+
+    adicionarlog();
+
+}
 
 // Exercicio 3 - Básico
+function exercicio03rep() {
 
-// let N = prompt("Digite um número.");
-// N = Number(N);
+    let N = prompt("Digite um número.");
+    N = Number(N);
 
-//     let soma = 0;
+    let soma = 0;
 
-// for (let contadora = 1; contadora <= N; contadora++) {
+    for (let contadora = 1; contadora <= N; contadora++) {
 
-//     soma = soma + contadora;
+        soma = soma + contadora;
 
-//     // console.log("A soma dos primeiros", N, "Números naturais é", soma);
+        // console.log("A soma dos primeiros", N, "Números naturais é", soma);
 
-// }
+    }
 
-// console.log(soma)
+    console.log(soma)
+
+    alert("Verificar exercício no console.")
+
+    adicionarlog();
+
+}
 
 // Exercicio 1 - Intermediário
+function exerciciointrep01() {
 
-    // for (let contadora = 0; contadora <=50; contadora += 2) {
+    for (let contadora = 0; contadora <= 50; contadora += 2) {
 
-    //     console.log(contadora);
+        console.log(contadora);
 
-    // }
+    }
+
+    adicionarlog();
+
+}
 
 // Exercicio 2 - Intermediário
+function exerciciointrep02() {
 
-// let contadoraWhile = 50;
-// while (contadoraWhile >= 0) {
+let contadoraWhile = 50;
+while (contadoraWhile >= 0) {
 
-//     console.log(contadoraWhile);
+    console.log(contadoraWhile);
 
-//     contadoraWhile--;
+    contadoraWhile--;
 
-// }
+}
+
+    adicionarlog();
+
+}
+
+
